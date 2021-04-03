886898c9f96f488b8b514bc5af13bf6a18beadff
Richard Thomas <ri@chrd.uk>
2021-04-03 18:43:18 +0100

    say_both in level scene

--- applib/scenes/level.py
+++ applib/scenes/level.py
@@ -566,6 +566,14 @@ class LevelScene(object):
                 self.character_right.image_color = (255, 255, 255, 255)
                 self.character_left.image_color = (75, 75, 75, 255)
                 break
+            if command == 'say_both':
+                self.message_container.visible = True
+                self.message_area.text_update(value)
+                self.name_left.visible = bool(self.name_left.text)
+                self.name_right.visible = bool(self.name_right.text)
+                self.character_right.image_color = (255, 255, 255, 255)
+                self.character_right.image_color = (255, 255, 255, 255)
+                break
 
             if command == 'next_level':
                 if value:
