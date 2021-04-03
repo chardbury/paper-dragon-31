46650324422cd38cc984afd27b9a0682b5d4dab7
Richard Thomas <ri@chrd.uk>
2021-04-03 20:44:14 +0100

    More checks

--- scripts/checks.py
+++ scripts/checks.py
@@ -3,6 +3,7 @@ import os
 PROJECT_DIR_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 DATA_DIR_PATH = os.path.join(PROJECT_DIR_PATH, 'data')
 SCENES_DIR_PATH = os.path.join(DATA_DIR_PATH, 'scenes')
+
 VALID_SCENE_DIRECTIVES = {
     'end_game',
     'next_level',
@@ -17,19 +18,23 @@ VALID_SCENE_DIRECTIVES = {
     'set_right_name',
     'win_game',
 }
+
 IGNORE_SCENE_NAMES = {
     'main_script.txt',
     'todo.txt',
 }
 
-for scene_file_name in os.listdir(SCENES_DIR_PATH):
-    if scene_file_name not in IGNORE_SCENE_NAMES:
-        scene_file_path = os.path.join(SCENES_DIR_PATH, scene_file_name)
-        for line in open(scene_file_path, 'r').readlines():
-            if len(line.strip()) > 0:
-                try:
-                    directive, value = map(str.strip, line.split(':', 1))
-                    assert directive in VALID_SCENE_DIRECTIVES, scene_file_name
-                except:
-                    print(scene_file_name)
+def check_scenes():
+    for scene_file_name in os.listdir(SCENES_DIR_PATH):
+        if scene_file_name not in IGNORE_SCENE_NAMES:
+            scene_file_path = os.path.join(SCENES_DIR_PATH, scene_file_name)
+            for line in open(scene_file_path, 'r').readlines():
+                if len(line.strip()) > 0:
+                    try:
+                        directive, value = map(str.strip, line.split(':', 1))
+                        assert directive in VALID_SCENE_DIRECTIVES, scene_file_name
+                    except:
+                        print(scene_file_name)
 
+if __name__ == '__main__':
+    check_scenes()
